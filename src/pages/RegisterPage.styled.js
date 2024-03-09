dc5001fd017046c81093062c476ae937bc09cc2a
Yurii Voloshyn <voloshyn9900@gmail.com>
2024-03-09 19:44:47 +0100

    commit9

--- src/pages/RegisterPage.styled.js
+++ src/pages/RegisterPage.styled.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  position: relative;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   width: 380px;
   height: 440px;
   background: transparent;
